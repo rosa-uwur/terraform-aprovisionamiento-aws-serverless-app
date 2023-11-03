e07e53bdf0e93e8b7309c494a2fddad550ad46fb
Yolanda Lopez Solis <yolita2402@gmail.com>
2023-11-03 16:12:40 -0600

    Add backend config

--- 1-database/terraform.tf
+++ 1-database/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  # backend "s3" {
-  #   bucket = ""
-  #   key    = "database/terraform.tfstate"
-  #   region = "us-east-1"
-  #   dynamodb_table = "" 
-  # }
+  backend "s3" {
+    bucket = ""
+    key    = "database/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "" 
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
